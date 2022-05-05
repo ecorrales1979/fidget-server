77a9f3ca0bda78a82cf0553eb872d94ed2c1e36a
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-04 22:27:10 -0300

    feat: ensure comment is not empty

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -20,6 +20,8 @@ export class SubmitFeedbackUseCase {
 
     if (!type) throw new Error("Type is required");
 
+    if (!comment) throw new Error("Comment is required");
+
     if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
       throw new Error("Invalid screenshot format");
     }
